abb220d656e0018dd27d5dc234fa1dee355854ea
Amish <amish.tufail2002@gmail.com>
2024-02-06 22:51:18 +0500

    Initial Commit

---
+++ CustomParallexPhotoEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomParallexPhotoEffect
+//
+//  Created by Amish Tufail on 06/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomParallexPhotoEffect/CustomParallexPhotoEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomParallexPhotoEffectApp.swift
+//  CustomParallexPhotoEffect
+//
+//  Created by Amish Tufail on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomParallexPhotoEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
